bbac591a740cb56f216965c2b3448e639da70638
fmp <fpagliughi@mindspring.com>
2017-12-29 10:40:37 -0500

    Initial working version

--- examples/redis_persist_pub.rs
+++ examples/redis_persist_pub.rs
@@ -1,23 +1,43 @@
-// async_publish.rs
+// redis_persist_pub.rs
 // 
-// Example/test for Paho MQTT Rust library.
+// Example/test for mqtt-redis.
+// 
+// This shows how to to use mqtt-redis with the Paho MQTT Rust library
+// in order to have Redis serve ss the persistence store for the messaging
+// application.
 //
 
-/*******************************************************************************
- * Copyright (c) 2017 Frank Pagliughi <fpagliughi@mindspring.com>
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * and Eclipse Distribution License v1.0 which accompany this distribution.
- *
- * The Eclipse Public License is available at
- *    http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at
- *   http://www.eclipse.org/org/documents/edl-v10.php.
- *
- * Contributors:
- *    Frank Pagliughi - initial implementation and documentation
- *******************************************************************************/
+// --------------------------------------------------------------------------
+// Copyright (c) 2017 Frank Pagliughi
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+// contributors may be used to endorse or promote products derived from this
+// software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
 
 extern crate log;
 extern crate env_logger;
@@ -25,14 +45,18 @@ extern crate env_logger;
 extern crate paho_mqtt as mqtt;
 extern crate mqtt_redis;
 
+use std::process;
 use mqtt_redis::RedisPersistence;
+
+// Use non-zero QoS to exercise message persistence
+const QOS: i32 = 1;
 
 // --------------------------------------------------------------------------
 
 fn main() {
 	env_logger::init().unwrap();
 
-	println!("Connecting to MQTT broker.");
+	println!("Creating the MQTT client");
 	// Create a client & define connect options
 	let persistence = RedisPersistence::new();
 
@@ -41,18 +65,22 @@ fn main() {
 			.user_persistence(persistence)
 			.finalize();
 
-	let cli = mqtt::AsyncClient::with_options(create_opts);
-	let conn_opts = mqtt::ConnectOptions::new();
+	let cli = mqtt::AsyncClient::new(create_opts).unwrap_or_else(|e| {
+		println!("Error creating the client: {:?}", e);
+		process::exit(1);
+	});
 
 	// Connect and wait for it to complete or fail
-	if let Err(e) = cli.connect(conn_opts).wait() {
-		println!("Unable to connect:\n\t{:?}", e);
-		::std::process::exit(1);
+	println!("Connecting to MQTT broker.");
+
+	if let Err(e) = cli.connect(None).wait() {
+		println!("Unable to connect: {:?}", e);
+		process::exit(1);
 	}
 
 	// Create a message and publish it
 	println!("Publishing a message to 'test' topic");
-	let msg = mqtt::Message::new("test", "Hello world!");
+	let msg = mqtt::Message::new("test", "Hello world!", QOS);
 	let tok = cli.publish(msg);
 
 	if let Err(e) = tok.wait() {
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,45 @@
 // lib.rs
+// 
+// Main library source file for 'mqtt-redis'.
 //
+// This is a small example of using Redis as the persistence store for the
+// Paho MQTT Rust client. The library allows any object to act as a 
+// persistence store for messages and other data. The object just needs
+// to implement the 'ClientPersistence' trait to service callbacks from 
+// the library. These callbacks map to the operations on a key/value 
+// store, so Redis is a perfect candidate to act as a store.
+//
+// --------------------------------------------------------------------------
+// Copyright (c) 2017 Frank Pagliughi
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+// contributors may be used to endorse or promote products derived from this
+// software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
 
 #[macro_use]
 extern crate log;
@@ -8,7 +48,7 @@ extern crate redis;
 
 extern crate paho_mqtt as mqtt;
 
-use redis::{Client, Commands, Connection, RedisResult, RedisError};
+use redis::{Client, Commands, Connection, RedisResult /*, RedisError*/};
 
 // --------------------------------------------------------------------------
 
@@ -61,19 +101,22 @@ impl mqtt::ClientPersistence for RedisPersistence
 		trace!("Client persistence [{}]: put key '{}'", self.name, key);
 		let conn = self.conn.as_ref().unwrap();	// TODO: Check for error?
 		let buf: Vec<u8> = buffers.concat();
+		debug!("Putting key '{}' with {} bytes", key, buf.len());
 		redis::cmd("HSET").arg(&self.name).arg(key).arg(buf).execute(conn);
 		Ok(())
 	}
 
-	fn get(&self, key: &str) -> mqtt::MqttResult<&[u8]> {
+	// Get the data buffer for the requested key.
+	fn get(&self, key: &str) -> mqtt::MqttResult<Vec<u8>> {
 		trace!("Client persistence [{}]: get key '{}'", self.name, key);
-		/*
-		match self.map.get(key) {
-			Some(v) => Ok(&v),
-			None => Err(mqtt::PERSISTENCE_ERROR)
+		let conn = self.conn.as_ref().unwrap();	// TODO: Check for error?
+		if let Ok(v) = conn.hget(&self.name, key) as RedisResult<Vec<u8>> {
+			debug!("Found key {} with {} bytes", key, v.len());
+			Ok(v)
 		}
-		*/
-		Err(mqtt::PERSISTENCE_ERROR)
+		else {
+			Err(mqtt::PERSISTENCE_ERROR)
+		}
 	}
 
 	fn remove(&mut self, key: &str) -> mqtt::MqttResult<()> {
@@ -81,30 +124,37 @@ impl mqtt::ClientPersistence for RedisPersistence
 		let conn = self.conn.as_ref().unwrap();	// TODO: Check for error?
 		if let Ok(res) = conn.hdel(&self.name, key) as RedisResult<usize> {
 			if res != 0 {
-				return Ok(());
+				debug!("Removed key: {}", key);
 			}
+			else {
+				debug!("Key not found (assuming OK): {}", key);
+			}
+			// Either way, if key is not in the store we report success.
+			return Ok(());
 		}
 		Err(mqtt::PERSISTENCE_ERROR)
 	}
 
-	fn keys(&self) -> mqtt::MqttResult<Vec<&str>> {
+	fn keys(&self) -> mqtt::MqttResult<Vec<String>> {
 		trace!("Client persistence [{}]: keys", self.name);
-		let mut kv: Vec<&str> = Vec::new();
-		/*
-		for key in self.map.keys() {
-			kv.push(key);
+		let conn = self.conn.as_ref().unwrap();	// TODO: Check for error?
+		if let Ok(v) = conn.hkeys(&self.name) as RedisResult<Vec<String>> {
+			debug!("Found keys: {:?}", v);
+			Ok(v)
 		}
-		*/
-		Ok(kv)
+		else {
+			warn!("Error looking for keys");
+			Err(mqtt::PERSISTENCE_ERROR)
+		}
 	}
 
 	fn clear(&mut self) -> mqtt::MqttResult<()> {
 		trace!("Client persistence [{}]: clear", self.name);
 		let conn = self.conn.as_ref().unwrap();	// TODO: Check for error?
-		if let Ok(res) = conn.del(&self.name) as RedisResult<usize> {
-			if res != 0 {
-				return Ok(());
-			}
+		if let Ok(_res) = conn.del(&self.name) as RedisResult<usize> {
+			// res==1 means hash/store deleted, 0 means it wasn't found.
+			// Either way, it's gone, so return success
+			return Ok(());
 		}
 		Err(mqtt::PERSISTENCE_ERROR)
 	}
