af8eb30c53859ea38471483cac359234ffdb0dcb
fpagliughi <fpagliughi@mindspring.com>
2020-11-24 09:39:15 -0500

    Updated to lasted Paho (develop). Fixed error checking. Reformat to get rid of tabs.

--- src/lib.rs
+++ src/lib.rs
@@ -90,131 +90,134 @@ pub struct RedisPersistence {
     /// The name of the Redis hash object.
     /// This is formed as a combination of the MQTT server name/address
     /// and the client ID string.
-	name: String,
+    name: String,
     /// The Redis client
-	client: Client,
+    client: Client,
     /// The connection to the Redis client.
     /// This is opened and closed on instruction from the MQTT client.
-	conn: Option<Connection>,
+    conn: Option<Connection>,
 }
 
 impl RedisPersistence {
     /// Create a new persistence object to connect to a local Redis server.
-	pub fn new() -> RedisPersistence {
-		RedisPersistence {
-			name: "".to_string(),
-			client: Client::open("redis://localhost/").unwrap(),
-			conn: None,
-		}
-	}
+    pub fn new() -> RedisPersistence {
+        RedisPersistence {
+            name: "".to_string(),
+            client: Client::open("redis://localhost/").unwrap(),
+            conn: None,
+        }
+    }
 }
 
 impl mqtt::ClientPersistence for RedisPersistence
 {
     /// Opena the connection to the Redis client.
-	fn open(&mut self, client_id: &str, server_uri: &str) -> mqtt::MqttResult<()> {
-		self.name = format!("{}:{}", client_id, server_uri);
+    fn open(&mut self, client_id: &str, server_uri: &str) -> mqtt::Result<()> {
+        self.name = format!("{}:{}", client_id, server_uri);
 
-		match self.client.get_connection() {
-			Ok(conn) => {
-				trace!("Redis persistence [{}]: open", self.name);
-				self.conn = Some(conn);
-				Ok(())
-			}
-			Err(e) => {
-				warn!("Redis persistence connect error: {:?}", e);
-				return Err(mqtt::PERSISTENCE_ERROR)
-			}
-		}
-	}
+        match self.client.get_connection() {
+            Ok(conn) => {
+                trace!("Redis persistence [{}]: open", self.name);
+                self.conn = Some(conn);
+                Ok(())
+            }
+            Err(e) => {
+                warn!("Redis persistence connect error: {:?}", e);
+                return Err(mqtt::PersistenceError)?
+            }
+        }
+    }
 
     /// Close the connection to the Redis client.
-	fn close(&mut self) -> mqtt::MqttResult<()> {
-		trace!("Client persistence [{}]: close", self.name);
-		self.conn = None;
-		Ok(())
-	}
+    fn close(&mut self) -> mqtt::Result<()> {
+        trace!("Client persistence [{}]: close", self.name);
+        self.conn = None;
+        Ok(())
+    }
 
     /// Store a persistent value to Redis.
-	/// We get a vector of buffer references for the data to store, which we
-	/// can concatenate into a single byte buffer to send to the server.
-	fn put(&mut self, key: &str, buffers: Vec<&[u8]>) -> mqtt::MqttResult<()> {
-		trace!("Client persistence [{}]: put key '{}'", self.name, key);
-		let conn = self.conn.as_ref().unwrap();	// TODO: Check for error?
-		let buf: Vec<u8> = buffers.concat();
-		debug!("Putting key '{}' with {} bytes", key, buf.len());
-		redis::cmd("HSET").arg(&self.name).arg(key).arg(buf).execute(conn);
-		Ok(())
-	}
+    /// We get a vector of buffer references for the data to store, which we
+    /// can concatenate into a single byte buffer to send to the server.
+    fn put(&mut self, key: &str, buffers: Vec<&[u8]>) -> mqtt::Result<()> {
+        trace!("Client persistence [{}]: put key '{}'", self.name, key);
+        let conn = self.conn.as_mut().ok_or(mqtt::PersistenceError)?;
+        let buf: Vec<u8> = buffers.concat();
+        debug!("Putting key '{}' with {} bytes", key, buf.len());
+        redis::cmd("HSET").arg(&self.name).arg(key).arg(buf).execute(conn);
+        Ok(())
+    }
 
-	/// Get the data buffer for the requested key.
+    /// Get the data buffer for the requested key.
     /// Although the value sent to the server was a collection of buffers,
     /// we can return them as a single, concatenated buffer.
-	fn get(&self, key: &str) -> mqtt::MqttResult<Vec<u8>> {
-		trace!("Client persistence [{}]: get key '{}'", self.name, key);
-		let conn = self.conn.as_ref().unwrap();	// TODO: Check for error?
-		if let Ok(v) = conn.hget(&self.name, key) as RedisResult<Vec<u8>> {
-			debug!("Found key {} with {} bytes", key, v.len());
-			Ok(v)
-		}
-		else {
-			Err(mqtt::PERSISTENCE_ERROR)
-		}
-	}
+    fn get(&mut self, key: &str) -> mqtt::Result<Vec<u8>> {
+        trace!("Client persistence [{}]: get key '{}'", self.name, key);
+        let conn = self.conn.as_mut().ok_or(mqtt::PersistenceError)?;
+        if let Ok(v) = conn.hget(&self.name, key) as RedisResult<Vec<u8>> {
+            debug!("Found key {} with {} bytes", key, v.len());
+            Ok(v)
+        }
+        else {
+            Err(mqtt::PersistenceError)
+        }
+    }
 
     /// Remove the value with the specified `key` from the store.
-	fn remove(&mut self, key: &str) -> mqtt::MqttResult<()> {
-		trace!("Client persistence [{}]: remove key '{}'", self.name, key);
-		let conn = self.conn.as_ref().unwrap();	// TODO: Check for error?
-		if let Ok(res) = conn.hdel(&self.name, key) as RedisResult<usize> {
-			if res != 0 {
-				debug!("Removed key: {}", key);
-			}
-			else {
-				debug!("Key not found (assuming OK): {}", key);
-			}
-			// Either way, if key is not in the store we report success.
-			return Ok(());
-		}
-		Err(mqtt::PERSISTENCE_ERROR)
-	}
+    fn remove(&mut self, key: &str) -> mqtt::Result<()> {
+        trace!("Client persistence [{}]: remove key '{}'", self.name, key);
+        let conn = self.conn.as_mut().ok_or(mqtt::PersistenceError)?;
+        if let Ok(res) = conn.hdel(&self.name, key) as RedisResult<usize> {
+            if res != 0 {
+                debug!("Removed key: {}", key);
+            }
+            else {
+                debug!("Key not found (assuming OK): {}", key);
+            }
+            // Either way, if key is not in the store we report success.
+            return Ok(());
+        }
+        Err(mqtt::PersistenceError)
+    }
 
     /// Return a collection of all the keys in the store for this client.
-	fn keys(&self) -> mqtt::MqttResult<Vec<String>> {
-		trace!("Client persistence [{}]: keys", self.name);
-		let conn = self.conn.as_ref().unwrap();	// TODO: Check for error?
-		if let Ok(v) = conn.hkeys(&self.name) as RedisResult<Vec<String>> {
-			debug!("Found keys: {:?}", v);
-			Ok(v)
-		}
-		else {
-			warn!("Error looking for keys");
-			Err(mqtt::PERSISTENCE_ERROR)
-		}
-	}
+    fn keys(&mut self) -> mqtt::Result<Vec<String>> {
+        trace!("Client persistence [{}]: keys", self.name);
+        let conn = self.conn.as_mut().ok_or(mqtt::PersistenceError)?;
+        if let Ok(v) = conn.hkeys(&self.name) as RedisResult<Vec<String>> {
+            debug!("Found keys: {:?}", v);
+            Ok(v)
+        }
+        else {
+            warn!("Error looking for keys");
+            Err(mqtt::PersistenceError)
+        }
+    }
 
     /// Remove all the data for this client from the store.
-	fn clear(&mut self) -> mqtt::MqttResult<()> {
-		trace!("Client persistence [{}]: clear", self.name);
-		let conn = self.conn.as_ref().unwrap();	// TODO: Check for error?
-		if let Ok(_res) = conn.del(&self.name) as RedisResult<usize> {
-			// res==1 means hash/store deleted, 0 means it wasn't found.
-			// Either way, it's gone, so return success
-			return Ok(());
-		}
-		Err(mqtt::PERSISTENCE_ERROR)
-	}
+    fn clear(&mut self) -> mqtt::Result<()> {
+        trace!("Client persistence [{}]: clear", self.name);
+        let conn = self.conn.as_mut().unwrap(); // TODO: Check for error?
+        if let Ok(_res) = conn.del(&self.name) as RedisResult<usize> {
+            // res==1 means hash/store deleted, 0 means it wasn't found.
+            // Either way, it's gone, so return success
+            return Ok(());
+        }
+        Err(mqtt::PersistenceError)
+    }
 
     /// Determines if the store for this client contains the specified `key`.
-	fn contains_key(&self, key: &str) -> bool {
-		trace!("Client persistence [{}]: contains key '{}'", self.name, key);
-		let conn = self.conn.as_ref().unwrap();	// TODO: Check for error?
-		if let Ok(res) = conn.hexists(&self.name, key) as RedisResult<usize> {
-			debug!("'contains' query returned: {:?}", res);
-			res != 0
-		}
-		else { false }
-	}
+    fn contains_key(&mut self, key: &str) -> bool {
+        trace!("Client persistence [{}]: contains key '{}'", self.name, key);
+        let conn = match self.conn.as_mut() {
+            Some(conn) => conn,
+            None => return false,
+        };
+        if let Ok(res) = conn.hexists(&self.name, key) as RedisResult<usize> {
+            debug!("'contains' query returned: {:?}", res);
+            res != 0
+        }
+        else { false }
+    }
 }
 
 /////////////////////////////////////////////////////////////////////////////
